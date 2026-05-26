c18c2aa7ddc87506ad81aa6bd3b1b7f9e3d2aa12
7gr9vhy7tc-commits <7gr9vhy7tc@privaterelay.appleid.com>
2026-05-26 16:56:00 +0900

    Update biz_streamlit.py

--- biz_streamlit.py
+++ biz_streamlit.py
@@ -30,12 +30,41 @@ st.latex('\int_a^b f(x) dx')
 """
 
 "#### :orange[이미지:st.image()]"
-st.image("./lectures/data/파이썬 설명.jpeg, caption="파이썬 설명 이미지", width=500)
+st.image("./data/파이썬 설명.jpeg", caption="파이썬 설명 이미지입니다.", width=500)
 
 "#### :orange[비디오:st.video()]"
-st.video("/Users/hyunju/Documents/2026_biz/lectures/data/17422066-hd_1080_1920_25fps.mp4")
+st.video("./data/17422066-hd_1080_1920_25fps.mp4")
 
 "#### :orange[오디오:st.audio()]"
-st.audio("/Users/hyunju/Documents/2026_biz/lectures/data/파이썬 음원.mp3")
+st.audio("./data/파이썬 음원.mp3")
 
 # 폴더 만들어서 넣어 줘야 올라감
+
+
+"#### :orange[Pandas 데이터프레임]"
+import pandas as pd
+df = pd.DataFrame({
+    '이름': ['홍길동', '김철수', '박영희'],
+    '나이': [25, 30, 22],
+    '직업': ['학생', '회사원', '프리랜서'] 
+})
+st.dataframe(df)
+
+'''
+
+|이름|나이|직업|
+|---|---|---|
+|홍길동|25|학생|
+|김철수|30|회사원|
+|박영희|22|프리랜서|
+
+'''
+
+'#### :orange[지표:st.metric()]'
+col1, col2, col3, col4 = st.columns(4)
+col1.metric("온도", "25°C", "2°C")
+col1.write("이건 온도입니다.")
+col2.metric("습도", "60%", "-5%")
+col2.write("이건 습도입니다.")
+col3.metric("풍속", "10km/h", "1km/h")
+col4.metric("강수량", "5mm", "0mm")
